0e5e4456c1c749d4ebcde8d4ac5feed82511603d
filza-150 <filzanaeem987@gmail.com>
2024-10-13 00:15:42 +0500

    first commit

---
+++ new.js
@@ -0,0 +1,15 @@
+// chap 1
+// Question No.1
+alert("Hello")
+
+// Question No.2
+alert("Error! Please enter a valid password.")
+
+// Question No.3
+alert("Welcome to JS Land... \n Happy Coding!")
+
+// Question No.4
+alert("Welcome to JS Land...")
+alert("Happy Coding! \n Prevent this page from creating additional dialogs.")
+
+
