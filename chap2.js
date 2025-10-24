ae52d7c0d910c5893def72cfacb42fc1fc8d9fbe
Filza-150 <filzanaeem987@gmail.com>
2025-10-24 13:22:49 +0500

    added chap

---
+++ chap2.js
@@ -0,0 +1,38 @@
+// chap 2
+// Question No.1
+let username;
+
+// Question No.2
+let myName = "Filza Naeem";
+
+//  question No.3
+let message = "Hello World";
+alert(message);
+
+// question No.4
+let name = "Filza Naeem";
+alert(name);
+let age = "15"
+alert(age);
+let course = "web development";
+alert(course);
+
+// question No.5
+let pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+alert(pizza);
+
+// question No.6
+let email = "filzanaeem987@gmail.com";
+alert("my email address is " + email);
+
+// question No.7
+let book = "A smarter \n way to learn JavaScript";
+alert("i am trying to learn from the Book " + book);
+
+// question No.8
+document.write("yah! I can write HTML content through JavaScript");
+
+// question No.9
+
+
+
